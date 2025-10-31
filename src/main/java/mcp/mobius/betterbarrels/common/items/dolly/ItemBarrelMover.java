281099cc783cf41ce051283b7ca14fc85dc795e7
Tom Prince <tom.prince@hocat.ca>
2025-10-31 17:04:20 -0600

    Allow compressed (and infinity) chests to be dollied. (#46)

--- src/main/java/mcp/mobius/betterbarrels/common/items/dolly/ItemBarrelMover.java
+++ src/main/java/mcp/mobius/betterbarrels/common/items/dolly/ItemBarrelMover.java
@@ -121,6 +121,8 @@ public class ItemBarrelMover extends Item {
 
         classExtensionsNames.add("ganymedes01.etfuturum.tileentities.TileEntityBarrel");
 
+        classExtensionsNames.add("wanion.avaritiaddons.block.chest.TileEntityAvaritiaddonsChest");
+
         for (String s : classExtensionsNames) {
             try {
                 classExtensions.add(Class.forName(s));
